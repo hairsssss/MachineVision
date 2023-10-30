7eff9cf6978f903b64ee2b6a1c42eaed846edbc1
Yang zhen <2456630607@qq.com>
2023-10-30 17:37:10 +0800

    update

---
+++ class/Day9/Day9_1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Day_9_1 {
+    internal class Program {
+        static void Main(string[] args) {
+
+
+            Array intArray = Array.CreateInstance(typeof(int), 5);
+            for (int i = 0; i < intArray.Length; i++) {
+                intArray.SetValue(i + 1, i);
+
+            }
+            //foreach (var item in intArray)
+            //{
+            //    Console.WriteLine(item.GetType());
+            //}
+            Console.WriteLine(Array.IndexOf(intArray, 1));
+
+
+            //  Console.WriteLine(Array1.IndexOf(intArray, 2));
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+
+    internal class Array1 {
+
+        public static int IndexOf(int[] array, int value) {
+
+
+            Console.WriteLine(value.GetType());
+            for (int i = 0; i < array.Length; i++) {
+
+                Console.WriteLine(array.GetValue(i).GetType());
+
+                if (array[i] == value) {
+                    return i;
+                    //  break;
+                }
+
+            }
+
+            return -1;
+
+        }
+
+
+        public static int IndexOf(string[] array, string value) {
+
+
+            Console.WriteLine(value.GetType());
+            for (int i = 0; i < array.Length; i++) {
+
+                Console.WriteLine(array.GetValue(i).GetType());
+
+                if (array[i] == value) {
+                    return i;
+                    //  break;
+                }
+
+            }
+
+            return -1;
+
+        }
+
+
+    }
+}
