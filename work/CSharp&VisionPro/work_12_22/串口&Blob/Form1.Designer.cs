7980cb4fc1dc5486da30c0c1df5113da932fd3db
Yang zhen <2456630607@qq.com>
2023-12-22 22:27:16 +0800

    work_update

---
+++ work/CSharp&VisionPro/work_12_22/串口&Blob/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace 串口_Blob {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cogBlobEditV21 = new Cognex.VisionPro.Blob.CogBlobEditV2();
+            this.cogRecordDisplay1 = new Cognex.VisionPro.CogRecordDisplay();
+            this.takePhotoBtn = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.submitBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.cogBlobEditV21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cogRecordDisplay1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cogBlobEditV21
+            // 
+            this.cogBlobEditV21.Location = new System.Drawing.Point(26, 294);
+            this.cogBlobEditV21.MinimumSize = new System.Drawing.Size(489, 0);
+            this.cogBlobEditV21.Name = "cogBlobEditV21";
+            this.cogBlobEditV21.Size = new System.Drawing.Size(1178, 453);
+            this.cogBlobEditV21.SuspendElectricRuns = false;
+            this.cogBlobEditV21.TabIndex = 0;
+            // 
+            // cogRecordDisplay1
+            // 
+            this.cogRecordDisplay1.ColorMapLowerClipColor = System.Drawing.Color.Black;
+            this.cogRecordDisplay1.ColorMapLowerRoiLimit = 0D;
+            this.cogRecordDisplay1.ColorMapPredefined = Cognex.VisionPro.Display.CogDisplayColorMapPredefinedConstants.None;
+            this.cogRecordDisplay1.ColorMapUpperClipColor = System.Drawing.Color.Black;
+            this.cogRecordDisplay1.ColorMapUpperRoiLimit = 1D;
+            this.cogRecordDisplay1.DoubleTapZoomCycleLength = 2;
+            this.cogRecordDisplay1.DoubleTapZoomSensitivity = 2.5D;
+            this.cogRecordDisplay1.Location = new System.Drawing.Point(26, 12);
+            this.cogRecordDisplay1.MouseWheelMode = Cognex.VisionPro.Display.CogDisplayMouseWheelModeConstants.Zoom1;
+            this.cogRecordDisplay1.MouseWheelSensitivity = 1D;
+            this.cogRecordDisplay1.Name = "cogRecordDisplay1";
+            this.cogRecordDisplay1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("cogRecordDisplay1.OcxState")));
+            this.cogRecordDisplay1.Size = new System.Drawing.Size(601, 228);
+            this.cogRecordDisplay1.TabIndex = 1;
+            // 
+            // takePhotoBtn
+            // 
+            this.takePhotoBtn.Font = new System.Drawing.Font("宋体", 16F);
+            this.takePhotoBtn.Location = new System.Drawing.Point(852, 12);
+            this.takePhotoBtn.Name = "takePhotoBtn";
+            this.takePhotoBtn.Size = new System.Drawing.Size(175, 90);
+            this.takePhotoBtn.TabIndex = 2;
+            this.takePhotoBtn.Text = "拍照";
+            this.takePhotoBtn.UseVisualStyleBackColor = true;
+            this.takePhotoBtn.Click += new System.EventHandler(this.takePhotoBtn_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM2";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Font = new System.Drawing.Font("宋体", 16F);
+            this.submitBtn.Location = new System.Drawing.Point(852, 143);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(175, 90);
+            this.submitBtn.TabIndex = 3;
+            this.submitBtn.Text = "提交";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1233, 804);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.takePhotoBtn);
+            this.Controls.Add(this.cogRecordDisplay1);
+            this.Controls.Add(this.cogBlobEditV21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.PageLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.cogBlobEditV21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cogRecordDisplay1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Cognex.VisionPro.Blob.CogBlobEditV2 cogBlobEditV21;
+        private Cognex.VisionPro.CogRecordDisplay cogRecordDisplay1;
+        private System.Windows.Forms.Button takePhotoBtn;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button submitBtn;
+    }
+}
+
---
+++ work/CSharp&VisionPro/work_12_22/串口&Blob/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Cognex.VisionPro;
+using Cognex.VisionPro.Blob;
+using Cognex.VisionPro.ImageFile;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace 串口_Blob {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        //相机对象
+        ICogFrameGrabber frameGrabber = null;
+
+        //相机连接对象
+        ICogAcqFifo fifo = null;
+
+        //采图对象
+        ICogImage image = null;
+
+        //Blob工具
+        CogBlobTool blobTool = null;
+
+        //加载vpp文件
+        private void LoadVpp() {
+            string path = Directory.GetCurrentDirectory() + @"\Vpp\Blob.vpp";
+
+            // 反序列化vpp文件 得到Blob对象
+            blobTool = (CogBlobTool)CogSerializer.LoadObjectFromFile(path);
+            cogBlobEditV21.Subject = blobTool;
+        }
+
+        //相机初始化
+        private void InitCam() {
+            //获取所有已连接设备
+            CogFrameGrabbers frameGrabbers = new CogFrameGrabbers();
+
+            if (frameGrabbers.Count < 1) {
+                MessageBox.Show("未找到相机！");
+                return;
+            }
+
+            foreach (ICogFrameGrabber frame in frameGrabbers) {
+                frameGrabber = frame;
+                //创建采集接口
+                fifo = frameGrabber.CreateAcqFifo("Generic GigEVision (Mono)", CogAcqFifoPixelFormatConstants.Format8Grey, 0, true);
+
+                fifo.OwnedExposureParams.Exposure = 50;
+                fifo.Complete += Acq_Comlete;
+
+
+            }
+        }
+
+        //采图完成事件
+        private void Acq_Comlete(object sender, CogCompleteEventArgs e) {
+            int numReady, numPending;
+            bool busyVal;
+            try {
+                CogImage8Grey greyImage = new CogImage8Grey();
+                CogAcqInfo acqInfo = new CogAcqInfo();
+                fifo.GetFifoState(out numPending, out numReady, out busyVal);
+
+                if (numReady > 0) {
+                    greyImage = fifo.CompleteAcquireEx(acqInfo) as CogImage8Grey;
+                    image = greyImage;
+                    cogRecordDisplay1.Image = image;
+                    cogRecordDisplay1.Fit();
+
+                    SaveOriginImage(image);
+
+                    //设置blob输入图像
+                    cogBlobEditV21.Subject.InputImage = image;
+
+                }
+            } catch (Exception) {
+
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 保存原图
+        /// </summary>
+        /// <param name="image">传入的图片</param>
+        public void SaveOriginImage(ICogImage image) {
+            string path = Directory.GetCurrentDirectory() + "\\Image";
+
+            if (!Directory.Exists(path)) {
+                Directory.CreateDirectory(path);
+            }
+            string fileName = $"{DateTime.Now.ToString("yyyyMMddHHmmss")}.bmp";
+
+            //把ICogImage类型的图像 转成bitmap格式
+            Bitmap bmp = image.ToBitmap();
+            bmp.Save($"{path}\\{fileName}", System.Drawing.Imaging.ImageFormat.Bmp);
+
+        }
+
+        //窗口初始化
+        private void PageLoad(object sender, EventArgs e) {
+            LoadVpp();
+            InitCam();
+            //打开串口
+            serialPort1.Open();
+        }
+
+        private void takePhotoBtn_Click(object sender, EventArgs e) {
+            fifo.StartAcquire();
+        }
+
+
+        //串口连接
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e) {
+            //接收缓冲区数据的字节数
+            int size = serialPort1.BytesToRead;
+            //动态创建数组接收数据
+            byte[] buffer = new byte[size];
+            //读取数据
+            serialPort1.Read(buffer, 0, buffer.Length);
+            //字节数组转化字符串
+            string msg = Encoding.Default.GetString(buffer);
+            if (msg == "T1") {
+                fifo.StartAcquire();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            //关闭串口
+            if (serialPort1 != null && serialPort1.IsOpen)
+                serialPort1.Close();
+        }
+
+        //提交结果信息
+        private void submitBtn_click(object sender, EventArgs e) {
+            int resultStr = cogBlobEditV21.Subject.Results.GetBlobs().Count;
+
+            //byte[] send = Encoding.UTF8.GetBytes(resultStr.ToString() + "个");
+            //serialPort1.Write(send, 0, send.Length);
+
+            string testStr = "9个斑点";
+
+            // 使用 UTF-8 编码将字符串转换为字节数组
+            byte[] bytes = Encoding.UTF8.GetBytes(testStr);
+
+            // 将字节数组写入串口
+            serialPort1.Write(bytes, 0, bytes.Length);
+
+            // 添加延迟
+            System.Threading.Thread.Sleep(100); // 100 毫秒的延迟，根据实际情况调整
+
+        }
+    }
+}
